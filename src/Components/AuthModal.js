dd968d5ce03fce252215d8b9140bcc4644fd16d1
abhishekgarfield <abhishek2759@gmail.com>
2022-10-06 10:58:49 +0530

    starting

--- src/Components/AuthModal.js
+++ src/Components/AuthModal.js
@@ -1,20 +1,35 @@
-const AuthModal = ({ isLogin,setislogin , isAuthmodal, setIsauthmodal }) => {
+const AuthModal = ({ isLogin, setislogin, isAuthmodal, setIsauthmodal }) => {
   return (
     <div className="authmodal-main-cont">
-        <img src="https://i.imgur.com/J2pPJxt.png"/>
+      <img src="https://i.imgur.com/J2pPJxt.png" />
       <div className="authmodal-inner-cont">
-      <i className="fa fa-remove" onClick={()=>{
+        <i
+          className="fa fa-remove"
+          onClick={() => {
             setIsauthmodal(false);
-          }}> </i>
+          }}
+        >
+          {" "}
+        </i>
         <h1>{isLogin ? "Sign in" : "Sign up"}</h1>
         <input type="text" placeholder="Email" />
         <input type="password" placeholder="Password" />
         {!isLogin && <input type="password" placeholder="Confirm Password" />}
-        <input type="button" value={isLogin ? "Sign in" : "Sign up"} />
+        <input
+          type="button"
+          value={isLogin ? "Sign in" : "Sign up"}
+          onClick={() => {}}
+        />
         <p>
-          New to Netflix? <span onClick={()=>{
-            setislogin(false);
-          }}> Sign up now.</span>
+          New to Netflix?{" "}
+          <span
+            onClick={() => {
+              setislogin(false);
+            }}
+          >
+            {" "}
+            Sign up now.
+          </span>
         </p>
       </div>
     </div>
